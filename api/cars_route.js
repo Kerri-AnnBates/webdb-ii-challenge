8faeee4d2f9c1d8685a403b82b5a4dc607a4aa43
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-10 20:06:13 -0500

    set up DELETE request

--- api/cars_route.js
+++ api/cars_route.js
@@ -37,4 +37,23 @@ router.post('/', (req, res) => {
 
 })
 
+// DELETE
+router.delete('/:id', (req, res) => {
+   const carId = req.params.id;
+
+   db('cars')
+      .where({ id: carId })
+      .del()
+      .then(count => {
+         if (count > 0) {
+            res.status(200).json(count);
+         } else {
+            res.status(404).json({ message: "Unable to find car by that id." });
+         }
+      })
+      .catch(error => {
+         res.status(500).json({ message: 'Problem deleting the car' });
+      })
+})
+
 module.exports = router;
